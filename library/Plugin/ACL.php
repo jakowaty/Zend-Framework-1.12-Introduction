b32b74422e9e55c097601ecd596ec5e73f030225
jakowaty <herbalist@herbalist.hekko.pl>
2016-06-25 23:47:16 +0200

    Plugin_Acl populates user role

--- library/Plugin/ACL.php
+++ library/Plugin/ACL.php
@@ -19,17 +19,44 @@ Class Plugin_ACL extends Zend_Controller_Plugin_Abstract
         $this->__auth = Zend_Auth::getInstance();
     }
     
-    public function getCurrentRole () {
+    public function setCurrentRole () {
+        
         if (!$this->__auth->hasIdentity()) {
+            
             $this->currentRole = self::GUEST; 
+        
         } else {
-            $user = $this->__auth->getStorage()->read();
+            
+            $user = new Zend_Session_Namespace('Zend_Auth');
+            $role = $user->role;
+            
+            if (
+               ($role === self::GUEST) xor
+               ($role === self::USER) xor
+               ($role === self::ADMIN)
+               ) {
+                $this->currentRole = $role;
+            } else {
+                $this->currentRole = self::GUEST;
+            }
+            
         } 
+    }
+    
+    public function populateCurrentRole () {
+        $this->setCurrentRole();
+        
+        if(!$this->currentRole){
+            return false;
+        }
+        
+        $user = new Zend_Session_Namespace('Zend_Auth');
+        $user->role = $this->currentRole;
     }
     
     public function preDispatch(Zend_Controller_Request_Abstract $request)
     {
-        //$this->get
+        $this->populateCurrentRole();
     }
 
 
---
+++ zend1-12.sql-23-06-2016.sql
@@ -0,0 +1,118 @@
+-- MySQL dump 10.13  Distrib 5.7.12, for Linux (x86_64)
+--
+-- Host: localhost    Database: jak_zend_db
+-- ------------------------------------------------------
+-- Server version	5.7.12-0ubuntu1.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `jak_zend_db`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `jak_zend_db` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `jak_zend_db`;
+
+--
+-- Table structure for table `articles`
+--
+
+DROP TABLE IF EXISTS `articles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `articles` (
+  `articles_id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(80) NOT NULL,
+  `date_added` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `date_edited` timestamp NULL DEFAULT NULL,
+  `autor` varchar(45) NOT NULL,
+  `tags_id` varchar(50) NOT NULL,
+  `text` longtext NOT NULL,
+  PRIMARY KEY (`articles_id`,`tags_id`),
+  UNIQUE KEY `title_UNIQUE` (`title`),
+  KEY `fk_articles_1_idx` (`tags_id`),
+  CONSTRAINT `fk_articles_1` FOREIGN KEY (`tags_id`) REFERENCES `tags` (`tags_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='CREATE TABLE IF NOT EXISTS `jak_zend_db`.`articles` (\n  `articles_id` INT NOT NULL AUTO_INCREMENT,\n  `title` VARCHAR(80) NOT NULL,\n  `date_added` TIMESTAMP NOT NULL,\n  `date_edited` TIMESTAMP NOT NULL,\n  `autor` VARCHAR(45) NOT NULL,\n  `tags_id` VARCHAR(50) CHARACTER SET ''utf8'' COLLATE ''utf8_unicode_ci'' NOT NULL,\n  `text` LONGTEXT NOT NULL,\n  PRIMARY KEY (`articles_id`),\n  UNIQUE INDEX `title_UNIQUE` (`title` ASC),\n  CONSTRAINT `fk_articles_1`\n    FOREIGN KEY (``)\n    REFERENCES `jak_zend_db`.`tags` ()\n    ON DELETE NO ACTION\n    ON UPDATE NO ACTION)\nENGINE = InnoDB\nDEFAULT CHARACTER SET = utf8\nCOLLATE = utf8_unicode_ci';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `articles`
+--
+
+LOCK TABLES `articles` WRITE;
+/*!40000 ALTER TABLE `articles` DISABLE KEYS */;
+/*!40000 ALTER TABLE `articles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tags`
+--
+
+DROP TABLE IF EXISTS `tags`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tags` (
+  `tags_id` varchar(50) NOT NULL COMMENT 'Tagi, słowa kluczowe opisujące tematy .\n',
+  `name` varchar(50) NOT NULL,
+  `description` tinytext,
+  PRIMARY KEY (`tags_id`),
+  UNIQUE KEY `table1_id_UNIQUE` (`tags_id`),
+  UNIQUE KEY `name_UNIQUE` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tags`
+--
+
+LOCK TABLES `tags` WRITE;
+/*!40000 ALTER TABLE `tags` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tags` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `name` varchar(45) NOT NULL,
+  `hash` varchar(100) NOT NULL,
+  `role` varchar(6) NOT NULL DEFAULT 'guest',
+  PRIMARY KEY (`name`),
+  UNIQUE KEY `name_UNIQUE` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES ('jakowaty','9743a66f914cc249efca164485a19c5c','admin');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-06-25 13:50:42
