fbce3e3dbac2b21df7d16194892c77e4b06c051c
jakowaty <herbalist@herbalist.hekko.pl>
2016-06-25 17:05:05 +0200

    trying to obtain user role in ACL Plugin at predispatch()

--- library/Plugin/ACL.php
+++ library/Plugin/ACL.php
@@ -1,9 +1,7 @@
 <?php
 
 /**
- * Description of ACL
- *
- * @author jak_sieciowyaty
+ * 
  */
 Class Plugin_ACL extends Zend_Controller_Plugin_Abstract
 {
@@ -12,14 +10,26 @@ Class Plugin_ACL extends Zend_Controller_Plugin_Abstract
     protected $__auth;
     protected $currentRole;
     
+    const GUEST = 'guest';
+    const USER  = 'user';
+    const ADMIN = 'admin';
+    
     public function __construct(){
         $this->__acl = new Zend_Acl();
         $this->__auth = Zend_Auth::getInstance();
     }
     
+    public function getCurrentRole () {
+        if (!$this->__auth->hasIdentity()) {
+            $this->currentRole = self::GUEST; 
+        } else {
+            $user = $this->__auth->getStorage()->read();
+        } 
+    }
+    
     public function preDispatch(Zend_Controller_Request_Abstract $request)
     {
-        
+        //$this->get
     }
 
 
