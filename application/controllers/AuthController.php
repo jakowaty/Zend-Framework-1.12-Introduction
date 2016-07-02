571d811a2f4142bdba6dfa08018d133bc8c3441b
jakowaty <herbalist@herbalist.hekko.pl>
2016-07-02 14:48:09 +0200

    class Plugin_ACL refactored / reworked, AuthController temporary stable

--- application/controllers/AuthController.php
+++ application/controllers/AuthController.php
@@ -3,16 +3,11 @@
 /**
  * @class Auth
  */
-
-
-
 class AuthController extends Zend_Controller_Action
 {    
     protected $_auth;
     protected $_authAdapter;
-    protected $redirector;
     
-    //not sure if Ishould do this through constructor
     public function init(){
         $dbAdapter = Zend_Db_Table::getDefaultAdapter();
         $this->_auth = Zend_Auth::getInstance();
@@ -33,23 +28,18 @@ class AuthController extends Zend_Controller_Action
             }else{
                 $vals = $form->getValues();
                 $this->_authAdapter->setIdentity($vals['username']);
-                //I know this above isn't apropriate
-                //but this is about mvc/zend learn
+                //I know about salting and f.ex. Blowfish
+                //however this code is not about proper storing creds
+                //but about practicing frameworks
                 $this->_authAdapter->setCredential(md5($vals['password']));
                 $result = $this->_auth->authenticate($this->_authAdapter);
                 
-                
                 if($result->isValid()){
+                    $this->_auth
+                            ->getStorage()
+                            ->write($this->_authAdapter->getResultRowObject(['name', 'role']));
                     $this->view->success = 'Zalogowano';
-                    //$storage = $this->_auth->getStorage();
-                    //$storage->write($this->_authAdapter->getResultRowObject(null,'hash'));
-                    $user = $this->_authAdapter->getResultRowObject();
-                    $Zend_Auth = new Zend_Session_Namespace('Zend_Auth');
-                    $Zend_Auth->role = $user->role;
-                    
-                    //die;
                 }
-                
             }
         }
     }
--- library/Plugin/ACL.php
+++ library/Plugin/ACL.php
@@ -10,69 +10,70 @@ Class Plugin_ACL extends Zend_Controller_Plugin_Abstract
     protected $__auth;
     protected $currentRole;
     
+    protected $resources            = [
+        'artykuly',
+        'auth'
+    ];
+
+    protected $restrictedResources  = [
+        self::ADMIN => 'adder'
+    ];
+
     const GUEST = 'guest';
     const USER  = 'user';
     const ADMIN = 'admin';
     
-    protected function init(){
-        $this->__acl->addRole(new Zend_Acl_Role(self::GUEST));
-        $this->__acl->addRole(new Zend_Acl_Role(self::USER), self::GUEST);
-        $this->__acl->addRole(new Zend_Acl_Role(self::ADMIN));
-        $this->__acl->addResource(new Zend_Acl_Resource('adder'));
-        $this->__acl->deny(null, 'adder');
-        $this->__acl->allow(self::ADMIN, 'adder');
-    }
-    
-    public function __construct(){
-        $this->__acl = new Zend_Acl();
-        $this->__auth = Zend_Auth::getInstance();
-        $this->init();
+    protected function initACL(){
+        $this->setRoles();
+        $this->setResources();
+        $this->setPriviledges();
         $this->setCurrentRole();
     }
     
+    protected function setRoles(){
+        $this->__acl->addRole(new Zend_Acl_Role(self::GUEST));
+        $this->__acl->addRole(new Zend_Acl_Role(self::USER), self::GUEST);
+        $this->__acl->addRole(new Zend_Acl_Role(self::ADMIN));        
+    }
+    
+    protected function setResources(){
+        foreach ($this->restrictedResources as $resource) {
+            $this->__acl->addResource(new Zend_Acl_Resource($resource));
+        }
+    }
+    
+    protected function setPriviledges(){
+        foreach ($this->restrictedResources as $role => $resource) {
+            $this->__acl->allow($role, $resource);
+        }
+    }
+
+
+    public function __construct(){
+        $this->__acl    = new Zend_Acl();
+        $this->__auth   = Zend_Auth::getInstance();
+        $this->initACL();
+    }
+    
     public function setCurrentRole () {
-        
-        if (!$this->__auth->hasIdentity()) {
-            
-            $this->currentRole = self::GUEST; 
-        
-        } else {
-            
-            $user = new Zend_Session_Namespace('Zend_Auth');
-            $role = $user->role;
-            
+        if ($this->__auth->hasIdentity()) {
+            $role = $this->__auth->getIdentity()->role;
             if (
-               ($role === self::GUEST) xor
-               ($role === self::USER) xor
+               ($role === self::GUEST)  xor
+               ($role === self::USER)   xor
                ($role === self::ADMIN)
                ) {
                 $this->currentRole = $role;
             } else {
                 $this->currentRole = self::GUEST;
             }
-            
         } 
     }
-    
-/*    public function populateCurrentRole () {
-        $this->setCurrentRole();
-        
-        if(!$this->currentRole){
-            return false;
-        }
-        
-        $user = new Zend_Session_Namespace('Zend_Auth');
-        if ($user->role) {
-            $this->currentRole = $user->role;
-        }
-    }
- */   
+     
     public function preDispatch(Zend_Controller_Request_Abstract $request)
     {
-        $ctrl = $this->getRequest()->getControllerName();
-        
-        $allowed = $this->__acl->isAllowed($this->currentRole, 'adder');
-        die("controler: $ctrl ; ROLE: $this->currentRole");
+        $controller = $request->getControllerName();
+        $action     = $request->getActionName();
     }
 
 
