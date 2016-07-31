7c5c13467186c0f8e41c6b1958d8403507de8a3a
jakowaty <herbalist@herbalist.hekko.pl>
2016-07-31 14:20:09 +0200

    route regex

--- application/controllers/ArtykulyController.php
+++ application/controllers/ArtykulyController.php
@@ -2,21 +2,37 @@
 
 class ArtykulyController extends Zend_Controller_Action
 {
-    
+
     public function listaKategoriiAction()
     {
         $tags = new Application_Model_DbTable_Tags();
         $this->view->listTags = $tags->fetchAll(); 
     }
 
-    public function najnowszeArtykulyAction()
+    public function najnowszeartykulyAction()
     {
         $articles = new Application_Model_DbTable_Articles();
         $this->view->listArt = $articles->fetchAll();
+        $this->_helper->viewRenderer('najnowsze-artykuly');
+    }
+
+    public function najnowszeAction()
+    {
+        
+        $this->_helper->viewRenderer('art');
+        $this->view->param = $this->getParam('art_id');
+    }
+    
+    public function artykulShowAction()
+    {
+        $this->_helper->viewRenderer('art');
+        $this->view->param = 'b';
     }
 
 
 }
+
+
 
 
 
